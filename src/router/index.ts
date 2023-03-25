0b5470b191fe6ad144111f13f1220a2f79ab4b96
Umar Odeh Adejoh <stradox@gmail.com>
2023-03-25 20:58:12 +0100

    added routeguard to redirect to pu selection if no selected PU

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SelectPollingUnit from '../components/pages/SelectPollingUnit.vue'
+import { puSelected } from './routeGuards'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,8 @@ const router = createRouter({
     {
       path: '/submit/:puId',
       name: 'submit-result',
-      component: () => import('../components/pages/SubmitResult.vue')
+      component: () => import('../components/pages/SubmitResult.vue'),
+      beforeEnter: puSelected,
     }
   ]
 })
